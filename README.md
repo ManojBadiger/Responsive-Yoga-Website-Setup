20183e98f44c608009c6926f559bccfe186d0a4a
ManojBadiger <66901015+ManojBadiger@users.noreply.github.com>
2024-06-08 23:54:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Responsive Yoga/Medidation Website
+### Responsive Yoga/Medidation Website
+
+- Responsive Yoga/Medidation Website Design Using HTML CSS And JavaScript.
+- Contains scroll Reveal animation
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+💙 Join the channel to see more videos like this. [Code Genius](https://www.youtube.com/@codegenius02)
+
+![preview img](/preview.png)
---
+++ main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+// <!-- Scroll Reveal -->
